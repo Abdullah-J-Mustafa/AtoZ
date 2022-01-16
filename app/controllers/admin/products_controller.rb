67cbac784e4d4d30ca6857da8437ff2d8d54e1ca
Dev <dev@AbzXs-MacBook-Pro.local>
2022-01-16 10:42:34 -0800

    Configured Devise for User and especially for Admin

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,49 @@
+class Admin::ProductsController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product added!"
+      redirect_to admin_products_path
+    else
+      flash[:notice] = "Product can't be blank!"
+      render 'admin/products/new'
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'Product updated!'
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Product.destroy(params[:id])
+    flash[:notice] = 'Product removed!'
+    redirect_to admin_products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: 'products#index'
   resources :products, only: %i[index show]
+
+  namespace :admin do
+    root to: 'products#index'
+    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
 end
